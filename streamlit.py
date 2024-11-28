ce5275aa5834e80ea621c4f759610a157acfd860
Pastour97 <parsa.pmk.code@gmail.com>
2024-11-28 21:18:59 +0100

    Now streamlit outputs the transformer output (a list of values)

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,22 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+import pdfplumber
+from parser import preprocess_text
+from extractor import extract_kid_info
+
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI  # Use ChatOpenAI for chat models like GPT-4
+from langchain.chains import LLMChain
+
+import pprint
+import os
+import openai
+
+
+import sys
+print(sys.executable)
+
+
 
 # Page title
 st.title("PDF Text Extractor")
@@ -18,10 +35,16 @@ if uploaded_file is not None:
         for page in pdf_reader.pages:
             extracted_text += page.extract_text()
         
+        # with pdfplumber.open(pdf_path) as pdf:
+        #     for page in pdf.pages:
+        #         extracted_text.append(page.extract_text())
+        #     extracted_text = "\n".join(extracted_text)
         # Display extracted text
         st.subheader("Extracted Text:")
         if extracted_text.strip():
-            st.text_area("Text", extracted_text, height=400)
+            extracted_text = preprocess_text(extracted_text)
+            extracted_kid = extract_kid_info(extracted_text)
+            st.text_area("Text", str(extracted_kid), height=400)
         else:
             st.warning("No text could be extracted. The PDF might contain images instead of text.")
     except Exception as e:
